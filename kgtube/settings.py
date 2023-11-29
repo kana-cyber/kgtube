6c65276ba98464fa4f8e1c411b3d8211f3a45b47
kana-cyber <150323388+kana-cyber@users.noreply.github.com>
2023-11-30 02:20:08 +0600

    modified settings for deploy

--- kgtube/settings.py
+++ kgtube/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yx4%xt#q0%-do5z&w_^i#53n-kbeqpao%czh_1gpkcq^6^l3hu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -86,6 +86,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "kgtube_db", # название БД
+#         "USER": "postgres",
+#         "PASSWORD": "postgres",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -122,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_MEDIA = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
